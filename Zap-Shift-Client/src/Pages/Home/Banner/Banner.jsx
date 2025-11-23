e57507b94d1b8606100c97cc1dc95c06b95a6d17
sorkar5sourav <sorkar.s.sourav@gmail.com>
2025-11-23 19:28:30 +0600

    fake commit

--- Zap-Shift-Client/src/Pages/Home/Banner/Banner.jsx
+++ Zap-Shift-Client/src/Pages/Home/Banner/Banner.jsx
@@ -9,17 +9,22 @@ import { Link } from "react-router";
 
 const Banner = () => {
   return (
-    <Carousel autoPlay={true} infiniteLoop={true}>
-      <div>
+    <div className="relative">
+      <Carousel autoPlay={true} infiniteLoop={true} className="w-full mx-auto">
         <img src={bannerImg1} />
-      </div>
-      <div>
         <img src={bannerImg2} />
-      </div>
-      <div>
         <img src={bannerImg3} />
+      </Carousel>
+      <div className="absolute top-14/20 left-5/23 -translate-x-1/2 -translate-y-1/2 flex flex-row flex-wrap justify-center">
+        <Link to={"/track"} className="btn max-h-14 btn-primary rounded-full">
+          Trak Your Parcel
+        </Link>
+        <img src={Arrow} alt="" className="h-11" />
+        <Link to={"/rider-apply"} className="btn ml-4 max-h-14">
+          Be a Rider
+        </Link>
       </div>
-    </Carousel>
+    </div>
   );
 };
 
